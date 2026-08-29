41736dc7af7faebf696ad8fc9cbaba40bbcb97db
Smart-art30 <njengageorge73@gmail.com>
2026-08-29 15:15:22 +0300

    gdgggdd

--- smarthub/settings.py
+++ smarthub/settings.py
@@ -145,24 +145,34 @@ WSGI_APPLICATION = 'smarthub.wsgi.application'
 # DATABASE
 # =========================================================
 
-DATABASE_URL = config("DATABASE_URL", default="")
 
-if DATABASE_URL:
+
+IS_RENDER = config(
+    "RENDER",
+    default=False,
+    cast=bool
+)
+
+if IS_RENDER and config("DATABASE_URL", default=""):
+    
+
     DATABASES = {
         "default": dj_database_url.parse(
-            DATABASE_URL,
+            config("DATABASE_URL"),
             conn_max_age=600,
             conn_health_checks=True,
         )
     }
+
 else:
+   
+
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
 
 # =========================================================
 # PASSWORD VALIDATION
